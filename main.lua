39040b5242d397af587ba1cb8eaa20072d7be48e
Ironbeast917 <ironbeast917@gmail.com>
2025-02-18 17:09:04 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,6 @@
 ModelSpawner.SpawnModel("https://raw.githubusercontent.com/Ironbeast917/collaborationenginetesting/refs/heads/main/Map.glb", true, NewVector3(0, 0, 0), NewQuaternion(0, 0, 0, 0), nil)
 Entity.SetFloat("TestVar", 12.5234)
+
+function Update()
+    DebugLog(Entity.GetFloat("TestVar"))
+end
