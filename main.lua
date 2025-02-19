2770a75b73c729483c82a9e5c3595557fde711fb
Ironbeast917 <ironbeast917@gmail.com>
2025-02-18 18:06:21 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
-ModelSpawner.SpawnModel("https://raw.githubusercontent.com/Ironbeast917/collaborationenginetesting/refs/heads/main/Map.glb", true, NewVector3(0, 0, 0), NewQuaternion(0, 0, 0, 0), nil)
-Entity.CallFunction("Test", 12.412, "Hi", true)
-
 function Test(number, string, boolean)
     DebugLog(number)
     DebugLog(string)
     DebugLog(boolean)
 end
+
+ModelSpawner.SpawnModel("https://raw.githubusercontent.com/Ironbeast917/collaborationenginetesting/refs/heads/main/Map.glb", true, NewVector3(0, 0, 0), NewQuaternion(0, 0, 0, 0), nil)
+Entity.CallFunction("Test", 12.412, "Hi", true)
