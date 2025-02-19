8a19d28898b14ea4da73647373631f6b88c04729
Ironbeast917 <ironbeast917@gmail.com>
2025-02-18 20:36:00 -0600

    Update entity.lua

--- entity.lua
+++ entity.lua
@@ -1,4 +1,4 @@
 function FixedUpdate()
-    DebugLog(Entity)
+    DebugLog(Entity.GameObject)
     Entity.GameObject.Transform.Position = Entity.GameObject.Transform.Position + NewVector3(0, 0, 1) * Time.deltaTime
 end
