53714f31f7cfca8364b601a4a2f4f80b8f481571
Ironbeast917 <ironbeast917@gmail.com>
2025-02-18 20:05:25 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,11 @@
+entity = nil
+
 function Start()
     ModelSpawner.SpawnModel("https://raw.githubusercontent.com/Ironbeast917/collaborationenginetesting/refs/heads/main/Map.glb", true, NewVector3(0, 0, 0), NewQuaternion(0, 0, 0, 0), nil)
 
-    EntitySpawner.SpawnEntity(NewVector3(10, 0, 10), NewQuaternion(0, 0, 0, 0))
+    entity = EntitySpawner.SpawnEntity(NewVector3(10, 0, 10), NewQuaternion(0, 0, 0, 0))
+end
+
+function FixedUpdate()
+    entity.GameObject.Transform.Position = entity.GameObject.Transform.Position + NewVector3(0, 0, 1)
 end
